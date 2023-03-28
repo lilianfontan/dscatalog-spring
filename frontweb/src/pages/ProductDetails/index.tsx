39f25c69eef1a4d6548ad94ab8e75811c32db29f
lilianfontan <lilian.fontan@gmail.com>
2023-03-27 21:32:14 -0300

    Catalog - ProductDetails links

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,37 +1,47 @@
-import {ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
-import ProductPrice from 'components/ProductPrice';
+import { ReactComponent as ArrowIcon } from "assets/images/arrow.svg";
+import ProductPrice from "components/ProductPrice";
+import { Link } from "react-router-dom";
 
-import './styles.css'
+import "./styles.css";
 
 const ProductDetails = () => {
-    return (
-        <div className="product-details-container">
-            <div className="base-card product-details-card">
-                <div className="goback-container">
-                    <ArrowIcon />
-                    <h2>Voltar</h2>
-                </div>
-                <div className="row">
-                    <div className="col-xl-6">
-                        <div className="img-container">
-                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt='Nome do Produto'/>
-                        </div>
-                        <div className="name-price-container">
-                            <h1>Nome do Produto</h1>
-                            <ProductPrice price={2345.67} />
-                        </div>
-                    </div>
-                    <div className="col-xl-6">
-                        <div className="description-container">
-                            <h2> Descrição do Produto</h2>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque, libero. Exercitationem, iure. Consequuntur, ab! Harum aspernatur aliquid ad temporibus eum sapiente commodi. Incidunt tempora nulla, vero provident perferendis labore? Culpa.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="product-details-container">
+      <div className="base-card product-details-card">
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowIcon />
+            <h2>Voltar</h2>
+          </div>
+        </Link>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt="Nome do Produto"
+              />
             </div>
+            <div className="name-price-container">
+              <h1>Nome do Produto</h1>
+              <ProductPrice price={2345.67} />
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2> Descrição do Produto</h2>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque,
+                libero. Exercitationem, iure. Consequuntur, ab! Harum aspernatur
+                aliquid ad temporibus eum sapiente commodi. Incidunt tempora
+                nulla, vero provident perferendis labore? Culpa.
+              </p>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
