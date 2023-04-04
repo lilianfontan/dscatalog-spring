3d0fc10a5f5962e718ab4e6da91bd0dfb8893a3f
lilianfontan <lilian.fontan@gmail.com>
2023-04-04 20:33:17 -0300

    useState, useEffect, useParams

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,30 @@
 import { ReactComponent as ArrowIcon } from "assets/images/arrow.svg";
 import ProductPrice from "components/ProductPrice";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 import "./styles.css";
+import { Product } from "types/product";
+import axios from "axios";
+import { BASE_URL } from "util/requests";
+import { useEffect, useState } from "react";
+
+type UrlParams = {
+  productId: string;
+}
 
 const ProductDetails = () => {
+
+  const { productId } = useParams<UrlParams>();
+  
+  const [product, setProduct] = useState<Product>();
+ 
+  useEffect(() => {
+    axios.get(`${BASE_URL}/products/${productId}`)
+    .then(response => {
+        setProduct(response.data);
+    });
+  }, [productId])
+ 
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -18,23 +38,20 @@ const ProductDetails = () => {
           <div className="col-xl-6">
             <div className="img-container">
               <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="Nome do Produto"
+                src={product?.imgUrl}
+                alt={product?.name}
               />
             </div>
             <div className="name-price-container">
-              <h1>Nome do Produto</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2> Descrição do Produto</h2>
               <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque,
-                libero. Exercitationem, iure. Consequuntur, ab! Harum aspernatur
-                aliquid ad temporibus eum sapiente commodi. Incidunt tempora
-                nulla, vero provident perferendis labore? Culpa.
+                {product?.description}
               </p>
             </div>
           </div>
